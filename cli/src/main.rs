a0697cb2029a8c94205ea7e2ede9e6da34a2e77a
Harry Nguyen <sonntuet1997@gmail.com>
2024-02-18 16:08:10 +0700

    Init tracing

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,7 +1,14 @@
 mod options;
+mod telemetry;
 
+use crate::telemetry::init_telemetry;
 use crate::options::Options;
+
 use clap::Parser;
+use opentelemetry::trace::{Tracer};
+use tracing::{error};
+use opentelemetry::{global};
+
 
 /// Simple REST server
 #[derive(Parser, Debug)]
@@ -14,8 +21,15 @@ struct Args {
     config_file: String,
 }
 
+const EXPORTER_ENDPOINT: &str = "http://localhost:7281";
+const SERVICE_NAME: &str = "rust-api-server";
+
 #[tokio::main]
 async fn main() {
+    init_telemetry(SERVICE_NAME, EXPORTER_ENDPOINT);
+    global::shutdown_tracer_provider();
     let args = Args::parse();
-    Options::new(args.config_file.as_str()).expect("TODO: panic message");
+    Options::new(args.config_file.as_str()).map_err(|_e| {
+        error!("error occurs");
+    }).unwrap();
 }
---
+++ cli/src/telemetry.rs
@@ -0,0 +1,62 @@
+use opentelemetry::{global, KeyValue};
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::{Resource, runtime, trace};
+use opentelemetry_sdk::propagation::TraceContextPropagator;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{EnvFilter, Registry};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+/// Initializes OpenTelemetry for tracing in a web service.
+///
+/// This function initializes OpenTelemetry for distributed tracing in a web service.
+///
+/// # Arguments
+///
+/// * `service_name` - A string slice representing the name of the service.
+/// * `exporter_endpoint` - A string slice representing the endpoint of the exporter.
+///
+/// # Panics
+///
+/// This function will panic if it fails to initialize the tracer.
+///
+pub fn init_telemetry(service_name: &str, exporter_endpoint: &str) {
+    // Create a gRPC exporter
+    let exporter = opentelemetry_otlp::new_exporter()
+        .tonic()
+        .with_endpoint(exporter_endpoint);
+
+    // Define a tracer
+    let tracer = opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(exporter)
+        .with_trace_config(
+            trace::config().with_resource(Resource::new(vec![KeyValue::new(
+                opentelemetry_semantic_conventions::resource::SERVICE_NAME,
+                service_name.to_string(),
+            )])),
+        )
+        .install_batch(runtime::Tokio)
+        .expect("Error: Failed to initialize the tracer.");
+
+    // Define a subscriber
+    let subscriber = Registry::default();
+    // Level filter layer to filter traces based on level (trace, debug, info, warn, error)
+    let level_filter_layer = EnvFilter::try_from_default_env().unwrap_or(EnvFilter::new("INFO"));
+    // Layer for adding our configured tracer
+    let tracing_layer = tracing_opentelemetry::layer().with_tracer(tracer);
+    // Layer for printing spans to stdout
+    let formatting_layer = BunyanFormattingLayer::new(
+        service_name.to_string(),
+        std::io::stdout,
+    );
+
+    global::set_text_map_propagator(TraceContextPropagator::new());
+
+    subscriber
+        .with(level_filter_layer)
+        .with(tracing_layer)
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
+        .init()
+}
