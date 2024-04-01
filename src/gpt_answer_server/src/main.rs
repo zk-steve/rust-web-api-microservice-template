f44d9ebbe1ed472c28fb8a2d80067ac28cf69d8f
Harry Nguyen <sonntuet1997@gmail.com>
2024-04-01 09:22:43 +0700

    fix: refine

--- src/gpt_answer_server/src/main.rs
+++ src/gpt_answer_server/src/main.rs
@@ -5,17 +5,16 @@ use tonic::transport::Server;
 use common::grpc::gpt_answer::gpt_answer::gpt_answer_service_server::GptAnswerServiceServer;
 use common::loggers::telemetry::init_telemetry;
 use common::options::parse_options;
-use gpt_answer_server::controllers::gpt_answer::GptAnswerServer;
+use gpt_answer_server::controllers::gpt_answer::GptAnswerServiceImpl;
 use gpt_answer_server::options::Options;
 
-pub async fn init_grpc_server(options: Options) {
-    let server_endpoint = &options.server_endpoint;
-    let address = server_endpoint.parse().unwrap();
-    println!("Starting GPT Answer server at {}", server_endpoint);
+pub async fn serve(options: Options) {
+    let address = options.server_endpoint.parse().unwrap();
+    println!("Starting GPT Answer server at {}", options.server_endpoint);
 
-    let gpt_answer_server = GptAnswerServer::default();
+    let gpt_answer_service = GptAnswerServiceImpl::new("dummy_prop".to_string());
     Server::builder()
-        .add_service(GptAnswerServiceServer::new(gpt_answer_server))
+        .add_service(GptAnswerServiceServer::new(gpt_answer_service))
         .serve(address)
         .await
         .unwrap();
@@ -48,9 +47,9 @@ async fn main() {
         options.log.level.as_str(),
     );
 
-    let gpt_answer_server = tokio::spawn(init_grpc_server(options));
+    let server = tokio::spawn(serve(options));
 
-    tokio::try_join!(gpt_answer_server).expect("Failed to run servers");
+    tokio::try_join!(server).expect("Failed to run servers");
 
     global::shutdown_tracer_provider();
 }
